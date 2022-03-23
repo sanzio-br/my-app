da8fa6fdd29a7153e43a34cb3aea4be3be3c5267
sanzio-br <briansanzii@gmail.com>
2022-03-23 18:17:35 +0300

    new commit

--- src/components/Navbar/hambuger.js
+++ src/components/Navbar/hambuger.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 export default function Hambuger() {
     const [active, setActive] = useState(false)
     function toggle() {
@@ -10,11 +11,31 @@ export default function Hambuger() {
             <div className="nav-list">
                 <div onClick={toggle} className={active ? "active" : ""} id='hambuger'><div className="bar"></div></div>
                 <ul onClick={toggle} className={active ? "active" : ""}>
-                    <li onClick={toggle} className={active ? "active" : ""}><a onClick={toggle} className={active ? "active" : ""} href="#home" >HOME</a></li>
-                    <li onClick={toggle} className={active ? "active" : ""}><a onClick={toggle} className={active ? "active" : ""} href="#about">about</a></li>
-                    <li onClick={toggle} className={active ? "active" : ""}><a onClick={toggle} className={active ? "active" : ""} href="#projects">projects</a></li>
-                    <li onClick={toggle} className={active ? "active" : ""}><a onClick={toggle} className={active ? "active" : ""} href="#services">service</a></li>
-                    <li onClick={toggle} className={active ? "active" : ""}><a onClick={toggle} className={active ? "active" : ""} href="#contacts">contacts</a></li>
+                    <Link to='/'>
+                        <li
+                            onClick={toggle} className={active ? "active" : ""}>
+                            <a onClick={toggle} className={active ? "active" : ""} href="#home" >HOME</a>
+                        </li>
+                    </Link>
+                    <Link to='/about'>
+                        <li
+                            onClick={toggle} className={active ? "active" : ""}><a onClick={toggle} className={active ? "active" : ""} href="#about">about</a>
+                        </li>
+                    </Link>
+                    <Link to='/projects'>
+                        <li
+                            onClick={toggle} className={active ? "active" : ""}><a onClick={toggle} className={active ? "active" : ""} href="#projects">projects</a>
+                        </li>
+                    </Link>
+                    <Link to='/services'>
+                        <li onClick={toggle} className={active ? "active" : ""}><a onClick={toggle} className={active ? "active" : ""} href="#services">service</a>
+                        </li>
+                    </Link>
+                    <Link to='/contacts'>
+                        <li
+                            onClick={toggle} className={active ? "active" : ""}><a onClick={toggle} className={active ? "active" : ""} href="#contacts">contacts</a>
+                        </li>
+                    </Link>
                 </ul>
             </div>
         </div>
