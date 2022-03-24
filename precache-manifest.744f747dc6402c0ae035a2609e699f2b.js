e8b907dca74096a7e00eb559289c2d8f2d8219fc
sanzio-br <briansanzii@gmail.com>
2022-03-24 11:22:51 +0300

    Updates

---
+++ precache-manifest.744f747dc6402c0ae035a2609e699f2b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0821a6d226ba7932ec26",
+    "url": "/static/css/main.9e11f16c.chunk.css"
+  },
+  {
+    "revision": "0821a6d226ba7932ec26",
+    "url": "/static/js/main.0821a6d2.chunk.js"
+  },
+  {
+    "revision": "65cb4ccf59ef90ae00a0",
+    "url": "/static/js/1.65cb4ccf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a87ee6cc2face47ec97818225c1aebd7",
+    "url": "/static/media/profile.a87ee6cc.png"
+  },
+  {
+    "revision": "20442ddc3ae496270c8e3acddb2ed723",
+    "url": "/static/media/CURRICULUM VITAE.20442ddc.pdf"
+  },
+  {
+    "revision": "15ce8e59daa93134546b4c054e71dbc6",
+    "url": "/static/media/Santech.15ce8e59.png"
+  },
+  {
+    "revision": "98a6adc61f549d766cb50ece6ee1ccb3",
+    "url": "/static/media/portfolio.98a6adc6.png"
+  },
+  {
+    "revision": "ac3df493e958e8e5e24fc3c52efe7df0",
+    "url": "/static/media/tenzies.ac3df493.png"
+  },
+  {
+    "revision": "7a767471bf22828fced30693f9e43212",
+    "url": "/static/media/Notes.7a767471.png"
+  },
+  {
+    "revision": "08843ad53f32a328a0f4288f8c3ba206",
+    "url": "/index.html"
+  }
+];
